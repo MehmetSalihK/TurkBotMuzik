3ab2e7185a50e47b593ffe017830154a3b3c4649
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2020-05-19 10:26:19 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-discord_token = 'NzA4NTQ3NzYxNDgxNTgwNjU0.XsNhPQ.66QtKn4j7UDllDgnS1ub5cUGIrU'
-discord_token_dev = 'NzA4NTQ3NzYxNDgxNTgwNjU0.XsNhPQ.66QtKn4j7UDllDgnS1ub5cUGIrU'
+discord_token = 'key'
+discord_token_dev = 'key'
 genius_key = 'dfw6420X0aRgjLAtDA3ODQzFnI-Yt1Jb9dK9mmuIq_i-9OIDA3ODQhhFE__3jfg'
 spotify_secret = 'e9c41d9617fc44249d8fca779a6e1b7c'
 spotify_id = '9f1e753b102d4db8879827f56adf9681'
